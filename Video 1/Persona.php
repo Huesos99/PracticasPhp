94646f4534e015e12e5d7f7f65559dea291c53bb
JoseR <jdanel99@gmail.com>
2024-08-13 08:21:26 -0400

    first commit

---
+++ Video 1/Persona.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+final class Persona {
+    public $firstName; // Propiedad
+    public $lastName; // Propiedad
+
+    public function __construct($firstName, $lastName)
+    {
+        $this->firstName = $firstName;
+        $this->lastName =  $lastName;
+    }
+
+    public function fullName(): string // Metodo
+    {
+        return $this->firstName . " " . $this->lastName;
+    }
+}
+
+$persona1 = new Persona('José', 'Rodríguez'); // Al hacer un "new Persona" se crea una instancia de la clase
+
+$persona2 = new Persona('Juan', 'Mata');
+
+echo "{$persona1->fullName()} es amigo de {$persona2->fullname()}";
