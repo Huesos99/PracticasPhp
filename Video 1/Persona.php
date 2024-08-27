1135f4055434aaf02af4b55a1ec2f72d06259163
JoseR <jdanel99@gmail.com>
2024-08-27 13:24:17 -0400

    mejoras v1

--- Video 1/Persona.php
+++ Video 1/Persona.php
@@ -13,18 +13,22 @@ final class Persona {
         $this->lastName =  $lastName;
         $this->location =  $location;
     }
+
     public function firstName(): string
     {
         return $this->firstName;
     }
+
     public function location(): string
     {
         return $this->location;
     }
+
     public function fullName(): string // Metodo
     {
         return $this->firstName . " " . $this->lastName;
     }
+
 }
 
 $person1 = new Persona('José', 'Rodríguez', 'San José');
@@ -34,4 +38,3 @@ $person2 = new Persona('Juan', 'Mata','Tunapuy');
 echo "{$person1->fullName()} is friend of {$person2->fullname()}
        but {$person1->firstName()} live in {$person1->location()}
         and {$person2->firstName()} live in {$person2->location()}";
-
